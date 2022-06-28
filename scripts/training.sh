7d7d39862a6c639ccd481cebd416720cae40bfb6
Siddharth <sid@LAPTOP-7A3ND0RC.localdomain>
2022-06-28 22:57:40 +0530

    Added command to downgrade protobuf | Fixed path in training.sh

--- scripts/training.sh
+++ scripts/training.sh
@@ -1,3 +1,3 @@
 cd ..
-python3 tools/preprocess_data.py --input ./data/hindi-aggregated/c4-hindi-cleaned --output-prefix ./data/tokenized-data --vocab ./data/gpt2-vocab.json --merge-file gpt2-merges.txt --dataset-impl mmap --tokenizer-type GPT2BPETokenizer --append-eod --workers 16 && python3 ./deepy.py train.py -d configs small.yml local_setup.yml
+python3 tools/preprocess_data.py --input ./data/hindi-aggregated/c4-hindi-cleaned --output-prefix ./data/tokenized-data --vocab ./data/gpt2-vocab.json --merge-file ./data/gpt2-merges.txt --dataset-impl mmap --tokenizer-type GPT2BPETokenizer --append-eod --workers 16 && python3 ./deepy.py train.py -d configs small.yml local_setup.yml
 echo "Done :)"
